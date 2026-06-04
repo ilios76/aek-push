6f6ba0383cfe2be7f8b8c517a3dc17b3da327761
ilios76 <iliosis@gmail.com>
2026-06-04 12:29:14 +0300

    Update push.js

--- netlify/functions/push.js
+++ netlify/functions/push.js
@@ -1,26 +1,6 @@
-const fetch = require("node-fetch");
-
 exports.handler = async () => {
-  const appId = "232aeaed-673d-4076-b507-b2fc28a7c2ff";
-  const apiKey = "os_v2_app_emvov3lhhvahnnihwl6crj6c75fwggnuaekeqc57pevsc5gr3lu2cjsk5wgohzm3m4kwm5lfab4s5by4bsuqdkhguomwawbkvbt7hxq";
-
-  const response = await fetch("https://api.onesignal.com/notifications", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Basic ${apiKey}`
-    },
-    body: JSON.stringify({
-      app_id: appId,
-      included_segments: ["All"],
-      contents: { en: "AEK Corner: New update!" }
-    })
-  });
-
-  const data = await response.json();
-
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
+    body: "TEST 123"
   };
 };
