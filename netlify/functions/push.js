827aa5019719b6f3bf09496a2d77e1d403bdd1fc
ilios76 <iliosis@gmail.com>
2026-06-04 13:18:12 +0300

    Update push.js

--- netlify/functions/push.js
+++ netlify/functions/push.js
@@ -1,17 +1,46 @@
-const fetch = require("node-fetch");
+const { google } = require("googleapis");
 
 exports.handler = async () => {
-  const appId = "ΤΟ_APP_ID_ΣΟΥ";
-  const apiKey = "ΤΟ_REST_API_KEY_ΣΟΥ";
+  try {
+    const serviceAccount = JSON.parse(process.env.FCM_SERVICE_ACCOUNT);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      debug: {
-        appId,
-        apiKeyStartsWith: apiKey.substring(0, 10),
-        apiKeyLength: apiKey.length
+    const jwtClient = new google.auth.JWT(
+      serviceAccount.client_email,
+      null,
+      serviceAccount.private_key,
+      ["https://www.googleapis.com/auth/firebase.messaging"]
+    );
+
+    await jwtClient.authorize();
+
+    const projectId = serviceAccount.project_id;
+
+    const message = {
+      message: {
+        token: "ΤΟ_FCM_DEVICE_TOKEN_ΣΟΥ",
+        notification: {
+          title: "AEK Corner",
+          body: "Goal από FCM!"
+        }
       }
-    })
-  };
+    };
+
+    const response = await google
+      .firebasemessaging("v1")
+      .projects.messages.send({
+        parent: `projects/${projectId}`,
+        auth: jwtClient,
+        requestBody: message
+      });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
 };
