15306365c78f83889e0a0cca0cdee23c8aedab36
ilios76 <iliosis@gmail.com>
2026-06-04 12:36:41 +0300

    Update push.js

--- netlify/functions/push.js
+++ netlify/functions/push.js
@@ -1,6 +1,17 @@
+const fetch = require("node-fetch");
+
 exports.handler = async () => {
+  const appId = "ΤΟ_APP_ID_ΣΟΥ";
+  const apiKey = "ΤΟ_REST_API_KEY_ΣΟΥ";
+
   return {
     statusCode: 200,
-    body: "TEST 123"
+    body: JSON.stringify({
+      debug: {
+        appId,
+        apiKeyStartsWith: apiKey.substring(0, 10),
+        apiKeyLength: apiKey.length
+      }
+    })
   };
 };
