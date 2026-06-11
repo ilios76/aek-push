7728282460398e4b7a26951dc5adcf47aad7c9f7
ilios76 <iliosis@gmail.com>
2026-06-11 15:50:35 +0300

    Update push.js

--- netlify/functions/push.js
+++ netlify/functions/push.js
@@ -10,8 +10,9 @@ exports.handler = async () => {
 
     // 2. Load Google News RSS feed (with cache-buster)
     const feed = await parser.parseURL(
-      "https://news.google.com/rss/search?q=%CE%91%CE%95%CE%9A&hl=el&gl=GR&ceid=GR:el&t=" + Date.now()
-    );
+  "https://url-to-rss-magic.lovable.app/api/public/aggregate?title=My+Aggregated+Feed&url=https%3A%2F%2Faek24hours.gr%2F%3Ffeed%3Drss2&url=https%3A%2F%2Fmonobala.gr%2Fcategory%2Fteams%2Fsl1%2Faek%2Ffeed%2F&url=https%3A%2F%2Fwww.aek1924.gr%2Fcategory%2Fpodosfairo%2Ffeed%2F&url=https%3A%2F%2Febasket.gr%2Fcontent%2F%CE%B11%2F%CE%B1%CE%B5%CE%BA%2Ffeed&url=https%3A%2F%2Fbasketa.gr%2Fcategory%2F%CF%8C%CE%BB%CE%B1-%CF%84%CE%B1-%CE%BD%CE%AD%CE%B1%2F%CE%BF%CE%BC%CE%AC%CE%B4%CE%B5%CF%82%2F%CE%B1%CE%B5%CE%BA%2Ffeed&url=https%3A%2F%2Fenwsi.gr%2Faek%2Fhandball%2Ffeed&url=https%3A%2F%2Faek1924.gr%2Fcategory%2Ferasitexniki%2Fhandball%2Ffeed&url=https%3A%2F%2Fonsports.gr%2Fomades%2Faek%3Fformat%3Dfeed%26type%3Drss&url=https%3A%2F%2Furl-to-rss-magic.lovable.app%2Fapi%2Fpublic%2Ffeed%2FaHR0cHM6Ly93d3cuc3BvcnQtZm0uZ3IvdGFnL2Flaw.xml&url=https%3A%2F%2Furl-to-rss-magic.lovable.app%2Fapi%2Fpublic%2Ffeed%2FaHR0cHM6Ly93d3cuZ2F6emV0dGEuZ3IvdGVhbXMvYWVr.xml&url=https%3A%2F%2Furl-to-rss-magic.lovable.app%2Fapi%2Fpublic%2Ffeed%2FaHR0cHM6Ly93d3cuc2RuYS5nci90ZWFtcy9hZWsvcG9kb3NmYWlybw.xml&url=https%3A%2F%2Furl-to-rss-magic.lovable.app%2Fapi%2Fpublic%2Ffeed%2FaHR0cHM6Ly93d3cuc3BvcnQyNC5nci90YWcvYWVrLw.xml&url=https%3A%2F%2Furl-to-rss-magic.lovable.app%2Fapi%2Fpublic%2Ffeed%2FaHR0cHM6Ly93d3cua2F0aGltZXJpbmkuZ3IvdGFnL2Flay8.xml&t=" + Date.now()
+);
+
 
     if (!feed.items || feed.items.length === 0) {
       return {
